37a0a55df15830569fe7e6e6293f61fd888fc1b0
Jihadmoazzam <gehad126@gmail.com>
2023-07-19 10:24:02 -0700

    array_iteratot

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
 
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array
+ * @array: the array
+ * @size: size of array
+ * @action: func poitner
+ *
+ * Return: nothing
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
